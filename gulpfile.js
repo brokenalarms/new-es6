68147865612ded6592b3c135f4c19399c4b48f6f
Daniel Lawrence <danilawr@cisco.com>
2016-03-02 16:55:31 -0800

    test

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp      = require('gulp');
+var GulpTasks = require('xd-gulp-tasks');
+//---------------------------------------------------------
+var gTask = new GulpTasks(gulp);
+// Load all tasks
+gTask.addAll();
+//---------------------------------------------------------
