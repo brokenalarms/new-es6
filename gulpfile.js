0859d6f3df76ba4c1b99153848b49ca6b497a772
Daniel Lawrence <danilawr@cisco.com>
2016-03-22 17:32:43 -0700

    fast commit

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp      = require('gulp');
-var GulpTasks = require('xd-gulp-tasks');
-//---------------------------------------------------------
-var gTask = new GulpTasks(gulp);
-// Load all tasks
-gTask.addAll();
-//---------------------------------------------------------
